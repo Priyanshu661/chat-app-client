92a23de6a6d2c5fb1be9578ed3778b0fe80050a3
Priyanshu Rathore <priyanshurathore0123@gmail.com>
2024-01-01 11:51:27 +0530

    added deployed server url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    SERVER_URL: "http://localhost:5000/api",
+    // SERVER_URL: "http://localhost:5000/api",
+    SERVER_URL: "http://13.200.241.129/api",
   },
 };
 
