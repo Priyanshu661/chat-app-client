8fffa9752d124610ed55053047ada2eba6a4d5f5
Priyanshu Rathore <priyanshurathore0123@gmail.com>
2024-01-02 23:09:20 +0530

    added cron job

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    SERVER_URL: "http://localhost:5000/api",
-    // SERVER_URL: "http://13.200.241.129/api",
+    // SERVER_URL: "http://localhost:5000/api",
+    SERVER_URL: "http://13.200.241.129/api",
 
   },
 };
